4919c5b5e2b96aa2ddbe419e262a65d1577be5ae
caiobacode <caioodsf@gmail.com>
2023-01-30 15:31:09 -0300

    feat: criando model "user"

---
+++ src/models/User.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, 
+  {
+    tableName: 'users',
+    underscored: true,
+    timestamps: false,
+  });
+  return User;
+}
