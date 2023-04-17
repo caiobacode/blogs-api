c3cd1df6fa8de25ec8ca3742d87f0538319a65d0
caiobacode <caioodsf@gmail.com>
2023-04-16 22:00:30 -0300

    teste

--- src/middlewares/schemas.js
+++ src/middlewares/schemas.js
@@ -3,6 +3,8 @@ const Joi = require('joi');
 const validateName = Joi.string().min(8);
 const validatePassword = Joi.string().min(6);
 
+// teste
+
 module.exports = {
   validateName,
   validatePassword,
